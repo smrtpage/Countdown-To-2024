62863c23fc1de79a8beee4f3018311435bdf1948
Igor-Lysak <lisakigor485@gmail.com>
2023-12-14 19:48:04 +0200

    code4you

---
+++ app.js
@@ -0,0 +1,27 @@
+const newYear = "1/1/2024";
+
+const daysEl = document.querySelector(".day");
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".seconds");
+
+function timeCountDown() {
+  const newDate = new Date();
+  const newYearDate = new Date(newYear);
+  const totalSeconds = (newYearDate - newDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.textContent = formatTime(days);
+  hourEl.textContent = formatTime(hours);
+  minuteEl.textContent = formatTime(minutes);
+  secondEl.textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time >= 10 ? time : `0${time}`;
+}
+timeCountDown();
+setInterval(timeCountDown, 1000);
